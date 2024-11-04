9b0ed4cfe31e004917ac2d240da071953857b172
krissyjames <kristina.ashley.cook@gmail.com>
2024-11-04 18:49:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,5 +23,12 @@ function App() {
     </div>
   );
 }
+}
 
 export default App;
+// Homework
+// Open your react application in your IDE
+// Navigate to your App.js file
+// Refactor your module to use a Class Component rather than a Functional Component
+// Tips: Review your imports and your exports. Also make sure you understand the render() function and what it returns.
+// Be prepared to answer the question: "What is the difference in importing React and importing Component"? What do you gain access to with each?
